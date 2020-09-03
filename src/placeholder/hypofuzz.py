be24ce9ba9129d1a6dde6f1203b77b0a441f180b
Zac-HD <zac.hatfield.dodds@gmail.com>
2020-09-03 11:51:29 +1000

    Add pypi placeholder

---
+++ src/placeholder/hypofuzz.py
@@ -0,0 +1,7 @@
+raise ModuleNotFoundError(
+    """
+This is a placeholder package to direct people to https://hypofuzz.com
+if they accidentally installed `hypofuzz` from PyPI, and to prevent
+typosquatting-style attacks from such accidents.
+"""
+)
