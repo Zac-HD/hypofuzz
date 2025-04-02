c756a64ef92d90797485026b3e99a20a135c8f0e
Liam DeVoe <orionldevoe@gmail.com>
2025-04-02 14:55:52 -0400

    try specifying base in react router

--- src/hypofuzz/frontend/src/App.tsx
+++ src/hypofuzz/frontend/src/App.tsx
@@ -11,7 +11,7 @@ import React from "react"
 
 export function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route element={<Layout />}>
           <Route
