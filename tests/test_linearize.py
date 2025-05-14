0fa3335274afdadd4b9c0a07a88b7e4320453e1b
Liam DeVoe <orionldevoe@gmail.com>
2025-05-14 02:19:15 -0400

    adjust test code

--- tests/test_linearize.py
+++ tests/test_linearize.py
@@ -3,7 +3,7 @@ from collections import defaultdict
 from typing import Optional
 
 import pytest
-from hypothesis import given, strategies as st
+from hypothesis import assume, given, strategies as st
 from hypothesis.internal.conjecture.data import Status
 
 from hypofuzz.dashboard import Test
@@ -34,6 +34,9 @@ def workers(*, uuid):
     )
 
 
+_shared_tests = [(b"database_key_" + str(i).encode(), f"nodeid_{i}") for i in range(10)]
+
+
 @st.composite
 def reports(
     draw, *, count_workers: Optional[int] = None, overlap: bool = False
@@ -41,6 +44,7 @@ def reports(
     # all of this min_size=len(uuids) etc is going to lead to terrible shrinking.
     # But the alternative of while draw(st.booleans()) will generate too-small
     # collections. Use `more` from hypothesis internals?
+    database_key, nodeid = draw(st.sampled_from(_shared_tests))
     uuids = draw(
         st.lists(
             st.uuids(version=4), min_size=count_workers or 0, max_size=count_workers
@@ -61,6 +65,10 @@ def reports(
     for uuid, (start_time, end_time) in zip(uuids, intervals):
         # reports from the same worker always have monotonically increasing
         # coverage, timestamps, and elapsed_time
+        # (TODO: we don't actually rely on timestamps being ordered. But our logic
+        # for unordered reports isn't correct yet either (we drop them, when we
+        # should correctly recompute/reinsert). Drop the .map(sorted) for timestamps
+        # when we implement this correctly.
         ninputs = draw(st.lists(st.integers(min_value=0)).map(sorted))
         timestamps = draw(
             st.lists(
@@ -73,7 +81,7 @@ def reports(
             st.lists(
                 # limit to reasonably-sized floats, we don't care about fp
                 # precision loss
-                st.floats(0, 2**30),
+                st.floats(0, 1_000_000),
                 min_size=len(ninputs),
                 max_size=len(ninputs),
             ).map(sorted)
@@ -85,9 +93,8 @@ def reports(
             report = draw(
                 st.builds(
                     Report,
-                    # TODO draw consistent database_key and nodeid per-worker
-                    database_key=st.just(b""),
-                    nodeid=st.just(""),
+                    database_key=st.just(database_key),
+                    nodeid=st.just(nodeid),
                     elapsed_time=st.just(elapsed_time),
                     timestamp=st.just(timestamp),
                     worker=workers(uuid=uuid),
@@ -123,12 +130,16 @@ def assert_reports_almost_equal(reports1, reports2):
 
 def _test_for_reports(reports) -> Test:
     reports_by_worker = defaultdict(list)
+    database_key = b""
+    nodeid = ""
     for report in sorted(reports, key=lambda r: r.elapsed_time):
         reports_by_worker[report.worker.uuid].append(report)
+        database_key = report.database_key
+        nodeid = report.nodeid
 
     return Test(
-        database_key=b"",
-        nodeid="",
+        database_key=database_key,
+        nodeid=nodeid,
         rolling_observations=[],
         corpus_observations=[],
         reports_by_worker=reports_by_worker,
@@ -164,13 +175,17 @@ def test_linearize_decomposes_with_addition(data):
     # (forgive the abuse of notation with add_report).
 
     reports_ = data.draw(reports(count_workers=1))
+    # size = 0 and size = 1 are trivial cases (and more importantly, mess up the
+    # nodeid in _test_for_reports, since there are no reports to draw the nodeid
+    # from).
+    assume(len(reports_) > 1)
     # the decomposition property is only actually true if we add reports in a sorted
     # order. This may not happen in practice, because e.g. reports from workers may
     # arrive out of order.
     # (e: this may no longer be true now that we correctly drop/handle out-of-order
     # reports in Test.add_report?)
     reports_ = sorted(reports_, key=lambda r: r.timestamp)
-    i = data.draw(st.integers(0, len(reports_)))
+    i = data.draw(st.integers(1, len(reports_) - 1))
     test1 = _test_for_reports(reports_)
 
     test2 = _test_for_reports(reports_[:i])
