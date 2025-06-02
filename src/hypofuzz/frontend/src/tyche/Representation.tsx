77ee9dad5cd0488d5b7b395840cee3d17adee8f4
Liam DeVoe <orionldevoe@gmail.com>
2025-06-02 01:48:35 -0400

    add pagination to textual observations

--- src/hypofuzz/frontend/src/tyche/Representation.tsx
+++ src/hypofuzz/frontend/src/tyche/Representation.tsx
@@ -1,9 +1,10 @@
-import { useEffect, useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 import hljs from "highlight.js/lib/core"
 import "highlight.js/styles/github.css"
 import python from "highlight.js/lib/languages/python"
 import { Observation } from "../types/dashboard"
 import { TycheSection } from "./TycheSection"
+import { Pagination } from "../components/Pagination"
 
 hljs.registerLanguage("python", python)
 
@@ -11,8 +12,11 @@ interface Props {
   observations: Observation[]
 }
 
+const perPage = 30
+
 export function Representation({ observations }: Props) {
   const observationsDivRef = useRef<HTMLDivElement>(null)
+  const [pageIndex, setCurrentPage] = useState(0)
 
   function reHighlight() {
     if (observationsDivRef.current) {
@@ -27,17 +31,22 @@ export function Representation({ observations }: Props) {
 
   useEffect(() => {
     reHighlight()
-  }, [observations])
+  }, [observations, pageIndex])
 
   if (observations.length === 0) {
     return null
   }
 
-  const representations = new Map<string, number>()
+  const rawRepresentations = new Map<string, number>()
   observations.forEach(observation => {
-    const rep = observation.representation
-    representations.set(rep, (representations.get(rep) || 0) + 1)
+    const repr = observation.representation
+    rawRepresentations.set(repr, (rawRepresentations.get(repr) || 0) + 1)
   })
+
+  const pageCount = Math.ceil(rawRepresentations.size / perPage)
+  const representations = Array.from(rawRepresentations.entries())
+    .sortKey(([rep, count]) => -count)
+    .slice(pageIndex * perPage, (pageIndex + 1) * perPage)
 
   return (
     <TycheSection
@@ -53,22 +62,49 @@ export function Representation({ observations }: Props) {
       }}
     >
       <div ref={observationsDivRef}>
-        {Array.from(representations.entries())
-          .sortKey(([rep, count]) => -count)
-          .map(([rep, count]) => (
-            // establish a new positioning context for the absolutely-positioned pill
-            <div
-              style={{ position: "relative" }}
-              className="tyche__representation__example"
-            >
-              <pre>
-                <code className="language-python">{rep}</code>
-              </pre>
-              {count > 1 && (
-                <div className="tyche__representation__example__count">x {count}</div>
-              )}
-            </div>
-          ))}
+        {pageCount > 1 && (
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "flex-end",
+              marginBottom: "12px",
+            }}
+          >
+            <Pagination
+              currentPage={pageIndex}
+              pageCount={pageCount}
+              onPageChange={setCurrentPage}
+            />
+          </div>
+        )}
+
+        {representations.map(([rep, count]) => (
+          // establish a new positioning context for the absolutely-positioned pill
+          <div
+            key={rep}
+            style={{ position: "relative" }}
+            className="tyche__representation__example"
+          >
+            <pre>
+              <code className="language-python">{rep}</code>
+            </pre>
+            {count > 1 && (
+              <div className="tyche__representation__example__count">x {count}</div>
+            )}
+          </div>
+        ))}
+
+        {pageCount > 1 && (
+          <div
+            style={{ display: "flex", justifyContent: "flex-end", marginTop: "12px" }}
+          >
+            <Pagination
+              currentPage={pageIndex}
+              pageCount={pageCount}
+              onPageChange={setCurrentPage}
+            />
+          </div>
+        )}
       </div>
     </TycheSection>
   )
