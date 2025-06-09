f0399f2cc78d97c8f51653d56c10280f630f0d6c
Liam DeVoe <orionldevoe@gmail.com>
2025-06-09 12:22:55 -0400

    refactor api logic for envvar

--- src/hypofuzz/frontend/src/utils/api.ts
+++ src/hypofuzz/frontend/src/utils/api.ts
@@ -1,18 +1,17 @@
-// prefer loading api responses from dashboard_state.json, and ask the api otherwise
 export async function fetchData<T>(endpoint: string): Promise<T | null> {
-  try {
+  if (import.meta.env.VITE_USE_DASHBOARD_STATE === "1") {
     const response = await fetch(
       new URL(/* @vite-ignore */ "dashboard_state/api.json", import.meta.url),
     )
     const data = JSON.parse(await response.text())
     return data[endpoint]
+  }
+
+  try {
+    const response = await fetch(`/api/${endpoint}`)
+    return await response.json()
   } catch (e) {
-    try {
-      const response = await fetch(`/api/${endpoint}/`)
-      return await response.json()
-    } catch (e) {
-      console.error(`Failed to fetch ${endpoint}:`, e)
-      return null
-    }
+    console.error(`Failed to fetch /api/${endpoint}`, e)
+    return null
   }
 }
