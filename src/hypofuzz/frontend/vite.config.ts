d1c283bb7e59c59b91346ab63b0422359bda3c4e
Liam DeVoe <orionldevoe@gmail.com>
2025-07-17 12:41:49 -0400

    add profiling build command

--- src/hypofuzz/frontend/vite.config.ts
+++ src/hypofuzz/frontend/vite.config.ts
@@ -2,33 +2,40 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 
-export default defineConfig({
-  plugins: [react(), svgr()],
-  resolve: {
-    alias: {
-      'src': '/src/'
-    }
-  },
-  // https://stackoverflow.com/a/79003101
-  css: {
-    preprocessorOptions: {
-      scss: {
-        api: 'modern-compiler'
+export default defineConfig(({ mode }) => {
+  let alias = {
+    'src': '/src/',
+  }
+  if (mode === 'profiling') {
+    alias['react-dom$'] = 'react-dom/profiling'
+    alias['scheduler/tracing'] = 'scheduler/tracing-profiling'
+  }
+  return {
+    plugins: [react(), svgr()],
+    resolve: {
+      alias: alias,
+    },
+    // https://stackoverflow.com/a/79003101
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: 'modern-compiler'
+        }
       }
-    }
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'react': ['react', 'react-dom', 'react-dom/client', 'react-router-dom'],
-          'd3': ['d3'],
-          'vendor': [
-            '@fortawesome/react-fontawesome',
-            '@fortawesome/free-solid-svg-icons',
-            'highlight.js',
-            'immutable',
-          ]
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            'react': ['react', 'react-dom', 'react-dom/client', 'react-router-dom'],
+            'd3': ['d3'],
+            'vendor': [
+              '@fortawesome/react-fontawesome',
+              '@fortawesome/free-solid-svg-icons',
+              'highlight.js',
+              'immutable',
+            ]
+          }
         }
       }
     }
