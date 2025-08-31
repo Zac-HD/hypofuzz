bbdc40b25554ef4476adbccb7f094c5487284b09
Liam DeVoe <orionldevoe@gmail.com>
2025-08-31 15:53:54 -0400

    reword

--- src/hypofuzz/frontend/src/pages/Test.tsx
+++ src/hypofuzz/frontend/src/pages/Test.tsx
@@ -71,7 +71,7 @@ function FailureCard({ failure }: { failure: Failure }) {
             {failure.observation.metadata.get("traceback")}
           </code>
         </pre>
-        <div className="failure__item__subtitle">Explicit reproducer</div>
+        <div className="failure__item__subtitle">Explicit reproduction</div>
         <pre>
           <code className="language-python">
             {failure.observation.metadata.get("reproduction_decorator") +
