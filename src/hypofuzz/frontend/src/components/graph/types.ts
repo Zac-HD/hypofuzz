a4bd4d7a72730f80f3369ddcd23222f360a4effd
Liam DeVoe <orionldevoe@gmail.com>
2025-09-02 11:13:45 -0400

    add more items to reports

--- src/hypofuzz/frontend/src/components/graph/types.ts
+++ src/hypofuzz/frontend/src/components/graph/types.ts
@@ -7,6 +7,8 @@ export class GraphReport {
     public linear_elapsed_time: number,
     public behaviors: number,
     public fingerprints: number,
+    public worker_uuid: string,
+    public timestamp_monotonic: number,
   ) {}
 
   static fromReport(nodeid: string, report: Report): GraphReport {
@@ -16,6 +18,8 @@ export class GraphReport {
       report.elapsed_time,
       report.behaviors,
       report.fingerprints,
+      report.worker_uuid,
+      report.timestamp_monotonic!,
     )
   }
 }
